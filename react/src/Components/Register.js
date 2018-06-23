17cfce1f086fd2d63ceee0894724c0adf252085b
Petra Vos <petravos70@gmail.com>
2018-06-23 14:32:51 +0200

    feat: add register and signin components

---
+++ react/src/Components/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class register extends Component {
+    render() {
+      return (
+        <div>
+
+        </div>
+      );
+    }
+  }
+
+  export default register;

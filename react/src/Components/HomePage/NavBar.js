6e3d1c710fdc797385b505cbaeef93f8bcd68cfa
KODISION\pvos <p.vos@kodision.nl>
2018-06-24 19:56:02 +0200

    refactor: add menu bar to file

--- react/src/Components/HomePage/NavBar.js
+++ react/src/Components/HomePage/NavBar.js
@@ -1,9 +1,59 @@
 import React, { Component } from "react";
-import "./App.css";
+import "../../Components/App.css";
+
+import { Menu, Segment } from "semantic-ui-react";
+
+const colors = ["teal"];
 
 class navBar extends Component {
+  state = {
+    activeItem: "home"
+  };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
-    return <div />;
+    const { color } = this.props;
+    const { activeItem } = this.state;
+    return (
+      <Segment inverted>
+        <Menu color={color} inverted pointing secondary>
+          <Menu.Item
+            name="Home"
+            active={activeItem === "Home"}
+            onClick={this.handleItemClick}
+          >
+            Home
+          </Menu.Item>
+
+          <Menu.Item
+            name="contact"
+            active={activeItem === "contact"}
+            onClick={this.handleItemClick}
+          >
+            Contact
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="signup"
+              active={activeItem === "signup"}
+              onClick={this.handleItemClick}
+            >
+              Sign Up
+            </Menu.Item>
+
+            <Menu.Item
+              name="Register"
+              active={activeItem === "Register"}
+              onClick={this.handleItemClick}
+            >
+              Register
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </Segment>
+    );
   }
 }
 

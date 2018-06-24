3ac537135cdeb5ebee213256362f4e7748e370f9
KODISION\pvos <p.vos@kodision.nl>
2018-06-24 12:33:17 +0200

    refactor: remove code no longer relevant

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,6 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }

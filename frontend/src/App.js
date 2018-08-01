5d2f813e296905cbf41af0cac599ed3c8e71e3d1
hixsav <hi.savis@gmail.com>
2018-08-01 12:52:06 +0100

    Updated seek route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>We React here!</h1>
-			</div>
+			<Router>
+				<div className="App">
+					<Navbar />
+					<Route exact path="/" component={Landing} />
+				</div>
+			</Router>
 		);
 	}
 }

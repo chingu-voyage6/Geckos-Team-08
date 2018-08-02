b8a2352699ba0d089a34c8f67549f9c0b417e9c9
hixsav <hi.savis@gmail.com>
2018-08-02 12:24:05 +0100

    Added redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,19 +10,23 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<div className="component">
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<div className="component">
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+						</div>
 					</div>
-				</div>
-			</Router>
+				</Router>
+			</Provider>
 		);
 	}
 }
